3beed58e81c2c95b2af45e566afa8cb32577de94
shinso-23 <skiesl00rd@gmail.com>
2026-05-17 00:12:10 +0100

    irc

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include "main.hpp"
 
+struct arguments
+{
+    size_t port;
+    std::string password;
+};
+
 int main(int ac, char **av)
 {
-    (void)ac;
+    arguments arg;
     (void)av;
-    if (!server())
-        throw std::runtime_error("error");
+    if (ac != 3)
+        throw std::runtime_error("Invalid arguments number!");
+    av++;
+    for (size_t i = 0; i < av[0][i]; i++)
+        if (!isdigit(av[1][i]))
+            throw std::runtime_error("Invalid Port");
+    arg.port = std::stoi(av[0]);
+    arg.password = av[1];
+    std::cout << "port is : " << arg.port << std::endl;
+    std::cout << "port is : " << arg.password << std::endl;
+    // // if (!server())
+    // //     throw std::runtime_error("error");
     return 0;
 }
