9827079aea9d3e32c818f94bfe39183f50038a30
HAMZA ELJARY <r33x@kali.0xr33x>
2026-06-02 05:55:42 +0100

    new version 05:55:42

--- CommandHandler.cpp
+++ CommandHandler.cpp
@@ -38,5 +38,5 @@ CommandHandler::CommandHandler(){
 }
 
 void CommandHandler::executeCommand(Message& msg, Client& client, Server& server){
-    if(_commands_map.find(msg.command) != _commands_map)
+    if(_commands_map.find(msg.command) != _commands_map.end())
 }
