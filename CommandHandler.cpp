ad6861e1aa7060665d94a7138c8e8b7587acde85
HAMZA ELJARY <r33x@kali.0xr33x>
2026-06-02 05:47:37 +0100

    new version 05:47:37

--- CommandHandler.cpp
+++ CommandHandler.cpp
@@ -37,6 +37,6 @@ CommandHandler::CommandHandler(){
     _commands_map["PING"] = &myPingCommand;
 }
 
-void CommandHandl::executeCommand(Message& msg, Client& client, Server& server){
-
+void CommandHandler::executeCommand(Message& msg, Client& client, Server& server){
+    
 }
