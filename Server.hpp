a5e287c550fa7e3b7a19588bb2a2e968431df9dd
HAMZA ELJARY <r33x@kali.0xr33x>
2026-06-02 07:16:19 +0100

    new version 07:16:19

--- Server.hpp
+++ Server.hpp
@@ -1,8 +1,15 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
-class Server{
+#include <string>
 
+class Server{
+    private:
+        std::string _password;
+    public:
+    
+        Server(std::string password);
+        void setPassword(std::string password);
 };
 
 #endif
