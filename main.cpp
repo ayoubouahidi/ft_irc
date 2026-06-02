fe49e081a3ac93d2fd8f0e8b3eadd4cf044a71e4
HAMZA ELJARY <r33x@kali.0xr33x>
2026-06-02 06:05:07 +0100

    new version 06:05:07

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 
 int main(){
     std::string _message;
-    std::string text = "PING :123";
+    std::string text = "PoNG :123";
     CommandHandler cmdH;
     Client mytestclient;
     Server mytestserver;
     Message Msg = cmdH.parseMessage(text);
-    cmdH.executeCommand(Msg, mytestclient, my);
+    cmdH.executeCommand(Msg, mytestclient, mytestserver);
 }
