45e54a39d4ae6f9d8c195aa25943ca353792b1b1
HAMZA ELJARY <r33x@kali.0xr33x>
2026-06-02 07:02:25 +0100

    new version 07:02:25

--- Client.hpp
+++ Client.hpp
@@ -16,7 +16,7 @@ class Client{
         std::string getUserName();
         std::string setUserName(std::string username);
         std::string getRealName();
-        std::string setRealName(std::string );
+        std::string setRealName(std::string realnam);
 };
 
 #endif
