f178a2331d7595ec4e05246f606e7671c9fe2b66
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2026-07-03 21:17:25 +0100

    some major function in channel done

--- Channel.cpp
+++ Channel.cpp
@@ -37,6 +37,116 @@ const std::map<int, Client*>& Channel::getClients() const {
     return _members;
 }
 
+
+int Channel::getMemberCount() const {
+    return _members.size();
+}
+
+const std::string& Channel::getPassword() const {
+    return _password;
+}
+
+const std::map<int, Client*>& Channel::getMembers() const {
+    return _members;
+}
+
+const std::map<int, Client*>& Channel::getOperators() const {
+    return _operators;
+}
+
+const std::string& Channel::getTopic() {
+    return _topic;
+}
+
+int Channel::getUserLimit() const {
+    return _userLimit;
+}
+
+bool Channel::isInviteOnly() const {
+    return _inviteOnly;
+}
+bool Channel::isTopicRestricted() const {
+    return _topicRestricted;
+}
+bool Channel::hasPassword() const {
+    return _hasPassword;
+}
+
+bool Channel::hasUserLimit() const {
+    return _hasUserLimit;
+}
+
+void Channel::addMember(Client* client) {
+    _members[client->getFd()] = client;
+}
+
+void Channel::removeMember(int fd) {
+    _members.erase(fd);
+    _operators.erase(fd);
+}
+
+bool Channel::isMember(int fd) const {
+    return _members.find(fd) != _members.end();
+}
+
+void Channel::addOperator(Client* client) {
+    _operators[client->getFd()] = client;
+}
+
+void Channel::removeOperator(int fd) {
+    _operators.erase(fd);
+}
+
+bool Channel::isOperator(int fd) const {
+    return _operators.find(fd) != _operators.end();
+}
+
+void Channel::addInvite(int fd) {
+    _inviteList.push_back(fd);
+}
+
+bool Channel::isInvited(int fd) const {
+    return std::find(_inviteList.begin(), _inviteList.end(), fd) != _inviteList.end();
+}
+
+void Channel::removeInvite(int fd) {
+    _inviteList.erase(std::remove(_inviteList.begin(), _inviteList.end(), fd), _inviteList.end());
+}
+
+void Channel::setTopic(const std::string& topic) {
+    _topic = topic;
+}
+
+void Channel::setInviteOnly(bool val) {
+    _inviteOnly = val;
+}
+
+void Channel::setTopicRestricted(bool val) {
+    _topicRestricted = val;
+}
+
+void Channel::setPassword(const std::string& password) {
+    _password = password;
+    _hasPassword = true;
+}
+
+void Channel::removePassword() {
+    _password.clear();
+    _hasPassword = false;
+}
+
+void Channel::setUserLimit(int limit) {
+    _userLimit = limit;
+    _hasUserLimit = true;
+}
+
+void Channel::removeUserLimit() {
+    _userLimit = 0;
+    _hasUserLimit = false;
+}
+
+
+
 void Channel::addClient(Client* client) {
     _members[client->getFd()] = client;
 }
@@ -46,3 +156,10 @@ void Channel::removeClient(Client* client) {
     _operators.erase(client->getFd());
 }
 
+void Channel::broadcast(const std::string& message, int excludeFd) {
+    for (std::map<int, Client*>::const_iterator it = _members.begin(); it != _members.end(); ++it) {
+        if (it->first != excludeFd) {
+            it->second->sendMsg(message);
+        }
+    }
+}
