255c379a444bdab812c98124a2a5027deacc502b
HAMZA ELJARY <r33x@kali.0xr33x>
2026-07-07 15:12:30 +0100

    0xR3EX

--- Join.cpp
+++ Join.cpp
@@ -1,75 +1,119 @@
 #include "CommandHandler.hpp"
 
+
+static std::string buildNamesList(Channel *chan)
+{
+    std::vector<Client *> members = chan->getClients();
+    std::string names;
+
+    for (size_t i = 0; i < members.size(); i++)
+    {
+        if (chan->isOperator(members[i]))
+            names += "@";
+        names += members[i]->getNickname();
+        if (i + 1 < members.size())
+            names += " ";
+    }
+    return names;
+}
+
+static void sendNamesReply(Client &client, const std::string &channel, Channel *chan)
+{
+    std::string names = buildNamesList(chan);
+    client.sendMsg(":ft_irc 353 " + client.getNickname() + " = " + channel + " :" + names);
+    client.sendMsg(":ft_irc 366 " + client.getNickname() + " " + channel + " :End of /NAMES list.");
+}
+
+static void joinSingleChannel(Client &client, const std::string &channel, const std::string &key, Server &server)
+{
+    if (channel.empty() || channel[0] != '#')
+    {
+        client.sendMsg(":ft_irc 403 " + client.getNickname() + " " + channel + " :No such channel");
+        return;
+    }
+
+    Channel *chan = server.getChannelByName(channel);
+
+    if (chan)
+    {
+        if (chan->isMember(&client))
+            return;
+
+        if (chan->isInviteOnly())
+        {
+            if (chan->isInvited(client.getFd()))
+                chan->removeInvite(client.getFd());
+            else
+            {
+                client.sendMsg(":ft_irc 473 " + client.getNickname() + " " + channel + " :Cannot join channel (+i)");
+                return;
+            }
+        }
+
+        if (!chan->getPassword().empty() && key != chan->getPassword())
+        {
+            client.sendMsg(":ft_irc 475 " + client.getNickname() + " " + channel + " :Cannot join channel (+k)");
+            return;
+        }
+
+        if (chan->getUserLimit() > 0 && chan->getMemberCount() >= chan->getUserLimit())
+        {
+            client.sendMsg(":ft_irc 471 " + client.getNickname() + " " + channel + " :Cannot join channel (+l)");
+            return;
+        }
+
+        chan->addClient(&client);
+        client.addChannel(channel);
+
+        std::string msgJoin = ":" + client.getNickname() + " JOIN :" + channel;
+        chan->broadcast(msgJoin);
+
+        sendNamesReply(client, channel, chan);
+    }
+    else
+    {
+        Channel *newChannel = new Channel(channel);
+
+        newChannel->addOperator(&client);
+        newChannel->addClient(&client);
+        client.addChannel(channel);
+        server.addChannel(channel, newChannel);
+
+        std::string msgJoin = ":" + client.getNickname() + " JOIN :" + channel;
+        newChannel->broadcast(msgJoin);
+
+        sendNamesReply(client, channel, newChannel);
+    }
+}
+
 void joinCommand(Client &client, std::vector<std::string> &params, Server &server)
 {
     if (params.empty())
     {
-        client.sendMsg("461 :Not enough parameters");
-        return;
-    }
-    std::string channel = params[0];
-    if (channel[0] != '#')
-    {
-        client.sendMsg("403 :No such channel");
+        client.sendMsg(":ft_irc 461 " + client.getNickname() + " JOIN :Not enough parameters");
         return;
     }
 
-    Channel *channel_name_already_tacked = server.getChannelByName(channel);
-    if (channel_name_already_tacked)
+    std::vector<std::string> channels;
+    std::vector<std::string> keys;
+
+    std::stringstream chanStream(params[0]);
+    std::string tmp;
+    while (std::getline(chanStream, tmp, ','))
+        channels.push_back(tmp);
+
+    if (params.size() >= 2)
     {
-        if (channel_name_already_tacked->isInviteOnly())
-        {
-            if (channel_name_already_tacked->isInvited(client.getFd()))
-            {
-                channel_name_already_tacked->removeInvite(client.getFd());
-            }
-            else
-            {
-                client.sendMsg("473 :Cannot join " + channel + "(+i)");
-                return;
-            }
-        }
-
-        if (channel_name_already_tacked->getUserLimit() > 0
-            && channel_name_already_tacked->getMemberCount() >= channel_name_already_tacked->getUserLimit())
-        {
-            client.sendMsg("471 :Cannot join " + channel + " (+l)");
-            return;
-        }
-
-        if (!channel_name_already_tacked->getPassword().empty())
-        {
-            if (params.size() < 2 || params[1] != channel_name_already_tacked->getPassword())
-            {
-                client.sendMsg("475 :Cannot join " + channel + " (+k)");
-                return;
-            }
-        }
-
-        channel_name_already_tacked->addClient(&client);
-        client.addChannel(channel);
-
-        std::string msgJoin = ":" + client.getNickname() + " JOIN :" + channel;
-        channel_name_already_tacked->broadcast(msgJoin);
-
-        client.sendMsg(":ft_irc 353 " + client.getNickname() + " = " + channel + " :<NAMES>");
-        client.sendMsg(":ft_irc 366 " + client.getNickname() + " " + channel + " :End of /NAMES list.");
+        std::stringstream keyStream(params[1]);
+        while (std::getline(keyStream, tmp, ','))
+            keys.push_back(tmp);
     }
-    else
+
+    for (size_t i = 0; i < channels.size(); i++)
     {
-        Channel *createNewChannel = new Channel(channel);
-        if (!createNewChannel)
-            return;
-
-        createNewChannel->addOperator(&client);
-        createNewChannel->addClient(&client);
-        client.addChannel(channel);
-        server.addChannel(channel, createNewChannel);
-
-        std::string msgJoin = ":" + client.getNickname() + " JOIN :" + channel;
-        createNewChannel->broadcast(msgJoin);
-
-        client.sendMsg(":ft_irc 353 " + client.getNickname() + " = " + channel + " :<NAMES>");
-        client.sendMsg(":ft_irc 366 " + client.getNickname() + " " + channel + " :End of /NAMES list.");
+        std::string key = "";
+        if (i < keys.size())
+            key = keys[i];
+        joinSingleChannel(client, channels[i], key, server);
     }
 }
